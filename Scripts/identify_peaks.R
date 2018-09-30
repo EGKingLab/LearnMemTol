8c815270b35e8212fe90a208adb063a469d23659
egking <kingeg@missouri.edu>
2018-09-30 16:06:56 -0500

    Update identification of DE genes within peaks

--- Scripts/identify_peaks.R
+++ Scripts/identify_peaks.R
@@ -23,12 +23,17 @@ for(kk in 1:3){
 qq <- poslist[,1:3]
 qq$LOD <- obs.LL[,kk]
 pp.set <- peak.i[[kk]]
-pp.set$up <- NA
 pp.set$lp <- NA
-pp.set$ug <- NA
+pp.set$up <- NA
 pp.set$lg <- NA
+pp.set$ug <- NA
 pp.set$ulod <- NA
 pp.set$llod <- NA
+pp.set$chrR6 <- NA
+pp.set$PposR6 <- NA
+pp.set$lpR6 <- NA
+pp.set$upR6 <- NA
+
 
 
 for(jj in 1:nrow(pp.set))
@@ -40,20 +45,58 @@ pp.set$ug[jj] <- ff$Gpos[2]
 pp.set$lg[jj] <- ff$Gpos[1]
 pp.set$ulod[jj] <- ff$LOD[2]
 pp.set$llod[jj] <- ff$LOD[1] 
+pp.set$chrR6[jj] <- coord.table[which(coord.table$R5chr==pp.set$chr[jj] & coord.table$R5pos==pp.set$Ppos[jj]),'R6chr']
+pp.set$PposR6[jj] <- as.numeric(coord.table[which(coord.table$R5chr==pp.set$chr[jj] & coord.table$R5pos==pp.set$Ppos[jj]),'R6pos'])
+
+pp.set$lpR6[jj] <- as.numeric(coord.table[which(coord.table$R5chr==pp.set$chr[jj] & coord.table$R5pos==ff$Ppos[1]),'R6pos'])
+pp.set$upR6[jj] <- as.numeric(coord.table[which(coord.table$R5chr==pp.set$chr[jj] & coord.table$R5pos==ff$Ppos[2]),'R6pos'])
+
 } 
 
-pp.set[,c('chr','Ppos')] <- merge(pp.set, coord.table, by.x=c('chr','Ppos'), by.y=c('R5chr','R5pos'),sort=FALSE)[,c('R6chr','R6pos')]
-pp.set[,c('up')] <- merge(pp.set, coord.table, by.x=c('chr','up'), by.y=c('R5chr','R5pos'),sort=FALSE)[,c('R6pos')]
-pp.set[,c('lp')] <- merge(pp.set[,c('chr','lp')], coord.table, by.x=c('chr','lp'), by.y=c('R5chr','R5pos'),sort=FALSE)[,c('R6pos')]
-pp.set$Ppos<- as.numeric(pp.set$Ppos)
-pp.set$up<- as.numeric(pp.set$up)
-pp.set$lp<- as.numeric(pp.set$lp)
+
 
 pp.set<-pp.set[order(pp.set$chr,pp.set$Ppos),]
 
 ci.peak[[kk]]<-pp.set
 }
 
+
+#clean up redundant peaks by hand 
+#EXAMINE ON PLOT
+#[[1]] 3R 17640000 
+#[[2]] 2R  3130000 
+#[[2]] 3L 15650000
+#[[2]] 3L 23440000
+#[[2]] 3R   820000
+
+#[[3]] 2R  2720000 BCI has problem
+#[[3]] 2R  7150000 
+#[[3]] 3L 16740000 BCI has problem
+#[[3]] 3R 13190000 BCI has problem
+#3R 15510000
+#3R 17960000
+#3R 20920000
+# 3R 23450000
+#re map with correction for large peak
+#for now, consider these:
+#2L 20110000
+#3L  9380000
+#3R 16810000
+
+
+
+ci.peak[[1]] <- ci.peak[[1]][-which(ci.peak[[1]]$chr=='3R' & ci.peak[[1]]$Ppos==17640000),]
+
+set.s <- c(which(ci.peak[[2]]$chr=='2R' & ci.peak[[2]]$Ppos==3130000),
+           which(ci.peak[[2]]$chr=='3L' & ci.peak[[2]]$Ppos==15650000),
+           which(ci.peak[[2]]$chr=='3L' & ci.peak[[2]]$Ppos==23440000),
+           which(ci.peak[[2]]$chr=='3R' & ci.peak[[2]]$Ppos==820000))
+ci.peak[[2]] <- ci.peak[[2]][-set.s,]
+
+set.s <- c(which(ci.peak[[3]]$chr=='2L' & ci.peak[[3]]$Ppos==20110000),
+           which(ci.peak[[3]]$chr=='3L' & ci.peak[[3]]$Ppos==9380000),
+           which(ci.peak[[3]]$chr=='3R' & ci.peak[[3]]$Ppos==16810000))
+ci.peak[[3]] <- ci.peak[[2]][set.s,]
 
 
 save(ci.peak,file="../Data/Peaks_wCIs.rda")
@@ -87,6 +130,7 @@ colnames(MemresSVorder)
 Mem_sig_genes <- as.data.frame(MemresSVorder)
 
 #DE genes thermal Tolerance 
+#are these only interaction?
 load(file="../Data/TTlrt_inter.Rda")
 str(TTlrt_inter)
 TTlrt_inter$FBgn <- rownames(TTlrt_inter)
@@ -95,21 +139,24 @@ colnames(TTlrt_inter)
 ThermTol_sig_genes <- as.data.frame(TTlrt_inter)
 
 
-#gene list form Fly Base
-gene_map_table <-read.table(file = "../Data/gene_map_table_fb_2015_03.tsv.gz", sep = '\t', header = FALSE, stringsAsFactors = FALSE)
-str(gene_map_table)
-colnames(gene_map_table) <- c('gname','FBgn','v3','cyt','pos')
+#gene list from Fly Base
 
-###
-####
-#is this truncating early? file has 244185 lines total
+gene_map_table <- read_lines(file = "../Data/gene_map_table_fb_2018_04.tsv",
+                skip = 6) %>% 
+  str_split(pattern = "\t", simplify = TRUE) %>% 
+  as_tibble() %>% 
+  filter(V1 == "Dmel")
+
+colnames(gene_map_table) <- c('spp','gname','FBgn','v3','cyt','pos')
+
+
 
 gene_map_table$chr <- str_split(gene_map_table$pos, ":",simplify=TRUE)[,1]
 temp.s1 <- str_split(gene_map_table$pos, ":",simplify=TRUE)[,2] %>% 
   str_split(fixed(".."),simplify=TRUE)
-gene_map_table$startp <- temp.s1[,1]
+gene_map_table$startp <- as.numeric(temp.s1[,1])
 
-gene_map_table$stopp <- str_split(temp.s1[,2],fixed("("),simplify = TRUE)[,1]
+gene_map_table$stopp <- as.numeric(str_split(temp.s1[,2],fixed("("),simplify = TRUE)[,1])
 
 str(gene_map_table)
 
@@ -127,24 +174,40 @@ colnames(Learn_gene_list_merged)
 
 #select specific columns
 
-Learn_gene_list_sub <- Learn_gene_list_merged[c(1,3,6,7,12:14)]
+Learn_gene_list_sub <- Learn_gene_list_merged[c(1,3,6,7,13:15)]
 colnames(Learn_gene_list_sub)
+Learn_gene_list_sub <- subset(Learn_gene_list_sub, padj <=0.05)
 
-Mem_gene_list_sub <- Mem_gene_list_merged[c(1,3,6,7,12:14)]
+Mem_gene_list_sub <- Mem_gene_list_merged[c(1,3,6,7,13:15)]
 colnames(Mem_gene_list_sub)
+Mem_gene_list_sub <- subset(Mem_gene_list_sub, padj <=0.05)
 
-ThermTol_gene_list_sub <- ThermTol_gene_list_merged[c(1,3,6,7,12:14)]
+
+ThermTol_gene_list_sub <- ThermTol_gene_list_merged[c(1,3,6,7,13:15)]
 colnames(ThermTol_gene_list_sub)
+ThermTol_gene_list_sub <- subset(ThermTol_gene_list_sub, padj <=0.05)
+
 
 #filter dataset to narrow down options
 #by chromosome number 
-TT_genes_3Rpeak <- ThermTol_gene_list_sub %>% filter(chr %in% c("3R"))
-str(TT_genes_3Rpeak)
+#TT_genes_3Rpeak <- ThermTol_gene_list_sub %>% filter(chr %in% c("3R"))
+#str(TT_genes_3Rpeak)
 
 #by startp number
-TT_genes_startp <-TT_genes_3Rpeak %>% filter(startp %in% 20000000:21100000)
-str(TT_genes_startp)
+#TT_genes_startp <-TT_genes_3Rpeak %>% filter(startp %in% 20000000:21100000)
+#str(TT_genes_startp)
 
 
 #TT_genes_FBgn <- TT_genes_startp %>% filter(FBgn %in% c("FBgn0001217"))
 #str(TT_genes_FBgn)
+
+#USE R6
+foc.peak <- ci.peak[[1]][1,]
+
+lw <- (which(Learn_gene_list_sub$chr==foc.peak$chr & 
+               ((Learn_gene_list_sub$startp <= foc.peak$upR6 & Learn_gene_list_sub$stopp >= foc.peak$lpR6))))  
+
+Learn_gene_list_sub[lw,]
+
+#make a plot showing locations & pvalues?
+
