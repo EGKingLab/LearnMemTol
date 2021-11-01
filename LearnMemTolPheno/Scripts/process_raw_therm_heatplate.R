1681a5375a848e7c25e17772af7a7dffc8c6bf0b
egking <kingeg@missouri.edu>
2021-11-01 09:13:50 -0500

    Update RNA processing

--- LearnMemTolPheno/Scripts/process_raw_therm_heatplate.R
+++ LearnMemTolPheno/Scripts/process_raw_therm_heatplate.R
@@ -100,39 +100,6 @@ for(ff in ffs) {
   cc<-cc+1
 }
 
-#Add 07-2 rerun
-#commented after incorporated into combined
-#TT72 <- readRDS(file="../ProcessedData/2021-07-02b_Fly_tracks.Rds")
-#TT72 <- bind_rows(TT72)
-
-#ffs <- unique(TT72$id)
-
-#for(ff in ffs) {
-#  tt1 <- TT72[TT72$id==ff,]
-#   
-# if(mean(tt1$likelihood) > 0.65) 
-# {
-#   
-#   Th.set <- data.frame( 'file'=character(length=1),
-#                         'incapacitation'= numeric(length=1), 
-#                         'Rvar'= numeric(length=1),
-#                         stringsAsFactors = FALSE)
-#   
-#   #remove low likelihood positions
-#   tt1 <- tt1[tt1$likelihood >= 0.6,]
-#   
-#   incap.i <- slideThermo(xx=tt1$x,tt=tt1$Second)
-#   
-#   #Th.set$Rvar <- slideRec(st <- incap.i, xx=tt1$x,tt=tt1$Second)
-#   Th.set$incapacitation <- tt1$Second[incap.i]
-#   Th.set$file <- tt1$id[1]
-#   
-#   ThermTol_HeatPlate <- rbind(ThermTol_HeatPlate, Th.set)
-#   
-# }
-# }
-##07-2
-
 ss <- ThermTol_HeatPlate$file %>%
   str_split("_",simplify=TRUE)
 
@@ -155,7 +122,8 @@ ThermTol_HeatPlate$cross <- ss3[,2]
 
 #saveRDS(ThermTol_HeatPlate, file=paste0("../ProcessedData/Incap_processed_",proj,".Rds"))
 
-
+proj <- "RNAi_1"
+ThermTol_HeatPlate <- readRDS(file=paste0("../ProcessedData/Incap_processed_",proj,".Rds"))
 chambers <- read_csv(file="../ProcessedData/HeatPlate_autotrack/Chambers_RNAi_round1.csv")
 chambers$FileName <- str_split(chambers$FileName,"_s", simplify=TRUE)[,1]
 
@@ -167,6 +135,8 @@ RNAi1 <- subset(RNAi1, FileName != "2021-04-06_RNAi-32871.3954-2_group1")
 
 filt <- RNAi1[which(is.na(RNAi1$incapacitation)),]
 
+proj <- "RNAi_2"
+ThermTol_HeatPlate <- readRDS(file=paste0("../ProcessedData/Incap_processed_",proj,".Rds"))
 
 chambers <- read_excel(path="../ProcessedData/HeatPlate_autotrack/RNAi_Val2_Chmb_positions.xlsx")
 
@@ -192,6 +162,11 @@ plot(RNAi2$Rvar, RNAi2$incapacitation)
 R1_c <- subset(RNAi1, Rvar >75)
 R2_c <- subset(RNAi2, Rvar >75)
 
+proj <- "RNAi_1"
+TTdat <- readRDS(file=paste0("../ProcessedData/Combined_tracks_",proj,".Rds"))
+TTdat <- as.data.table(TTdat)
+ThermTol_HeatPlate <- readRDS(file=paste0("../ProcessedData/Incap_processed_",proj,".Rds"))
+
 pdf(file="../Plots/RNA1_score.pdf",width=8, height=12)
 par(mfrow=c(3,2))
 
@@ -204,6 +179,10 @@ for(ff in R1_c$file)
 }
 
 dev.off()
+
+proj <- "RNAi_2"
+TTdat <- readRDS(file=paste0("../ProcessedData/Combined_tracks_",proj,".Rds"))
+TTdat <- as.data.table(TTdat)
 
 pdf(file="../Plots/RNA2_score.pdf",width=8, height=12)
 par(mfrow=c(3,2))
