9253a02f24867d87ea747fdd3f3b0e6bff8f3c14
egking <kingeg@missouri.edu>
2022-02-21 15:00:54 -0600

    process validation heatplate data

---
+++ LearnMemTolPheno/Scripts/process_raw_therm_heatplate_val.R
@@ -0,0 +1,218 @@
+
+slideThermo <- function(xx, tt)
+{
+  st <- 1
+  steps <- 1
+  width <- 60
+  tol <- 20
+  set <- 25
+  # disp <- 20
+  #dtol <-10
+  starts <- seq(from=1, to=(max(tt)-(width-1)),by=1)
+  ends <- seq(from = width, to = max(tt), by = 1)
+  counter <- 0
+  while((set > tol) & (counter <= length(starts)))
+  {
+    counter <- counter+1
+    if(length(xx[tt >= starts[counter] & tt < ends[counter]]) > 1)
+    {set <- var(xx[tt >= starts[counter] & tt < ends[counter]])}
+    st <- st+steps
+    
+  }
+  
+  if(counter <= length(starts))
+  {
+  return(min(tt[tt >= starts[counter] & tt < ends[counter]],na.rm=TRUE))  
+  }else{
+    return(max(tt,na.rm=TRUE))    
+  }
+}
+
+slideRec <- function(st, xx, tt)
+{
+  
+  steps <- 1 
+  width <- 60
+  
+  iis<-seq(from = tt[st], to = (max(tt)-width), by=steps)
+  vvs <- numeric(length(iis))
+  for(ii in 1:length(iis))
+  {
+    vvs[ii] <- var(xx[tt >= iis[ii] & tt < (iis[ii] + width)])
+  }
+  
+  return(max(vvs))  
+}
+
+
+
+library(tidyverse)
+library(data.table)
+library(readxl)
+
+
+
+#set as data frame ###change 
+ThermTol_HeatPlate<-data.frame('file'=character(length=0),
+                               'incapacitation'= numeric(length=0), 
+                               'Rvar'= numeric(length=0),
+                               stringsAsFactors = FALSE)
+
+proj <- "VAL"
+
+TTdat <- readRDS(file=paste0("../ProcessedData/HeatPlate_autotrack/Combined_tracks_",proj,".Rds"))
+TTdat <- as.data.table(TTdat)
+
+ffs <- unique(TTdat$id)
+cc <- 1
+
+for(ff in ffs) {
+  tt1 <- TTdat[TTdat$id==ff,]
+  
+  if(mean(tt1$likelihood) > 0.4) 
+  {
+    
+      Th.set <- data.frame( 'file'=character(length=1),
+                            'incapacitation'= numeric(length=1), 
+                            'Rvar'= numeric(length=1),
+                            stringsAsFactors = FALSE)
+      
+      lastpos <- tt1[(nrow(tt1)-200):nrow(tt1),]
+      lastpos <- lastpos[lastpos$likelihood >= 0.6,]
+
+      Th.set$Rvar <- max(lastpos$x) - min(lastpos$x)
+      
+      #remove low likelihood positions
+      tt1 <- tt1[tt1$likelihood >= 0.6,]
+      
+      incap.i <- slideThermo(xx=tt1$x,tt=tt1$Second)
+      
+      
+      #Th.set$Rvar <- slideRec(st <- incap.i, xx=tt1$x,tt=tt1$Second)
+      Th.set$incapacitation <- incap.i
+      Th.set$file <- tt1$id[1]
+      
+      ThermTol_HeatPlate <- rbind(ThermTol_HeatPlate, Th.set)
+    
+    
+  }
+  cat(cc,"\t")
+  cc<-cc+1
+}
+
+ss <- ThermTol_HeatPlate$file %>%
+  str_split("_",simplify=TRUE)
+
+#Date is fine
+ThermTol_HeatPlate$date <- ss[,1]
+ThermTol_HeatPlate$genotype <- NA
+ThermTol_HeatPlate$group <- NA
+ThermTol_HeatPlate$chamber <- NA
+
+
+
+#multiple separators used :(
+#split up 
+ww_a <- which(ss[,2]=="VAL")
+ww_b <- which(ss[,2]!="VAL")
+
+ss_extra <- str_split(ss[ww_b,2],"-", simplify=TRUE)
+unique(ss_extra[,1])
+unique(ss_extra[,2])
+unique(ss_extra[,3])
+unique(ss_extra[,4])
+
+length(ww_b)
+length(ss_extra[,2])
+
+ThermTol_HeatPlate$genotype[ww_b] <- ss_extra[,2]
+ThermTol_HeatPlate$group[ww_b] <- ss_extra[,3]
+
+ss_a_geno <- str_split(ss[ww_a,3],"-",simplify=TRUE)
+ThermTol_HeatPlate$genotype[ww_a] <- ss_a_geno[,1]
+ThermTol_HeatPlate$group[ww_a] <- ss_a_geno[,2]
+
+ss_chamb <- ThermTol_HeatPlate$file %>% 
+  str_sub(-3) %>%
+  str_split("_", simplify=TRUE)
+
+ThermTol_HeatPlate$chamber <- ss_chamb[,2]
+
+#saveRDS(ThermTol_HeatPlate, file=paste0("../ProcessedData/Incap_processed_",proj,".Rds"))
+
+chambers <- read_excel(path="../ProcessedData/HeatPlate_autotrack/VAL_alldata.xlsx")
+
+chambers_f <- str_split(chambers$filename,"_segmentation", simplify = TRUE)
+chambers$file <- paste0(chambers_f[,1],"_",chambers$chamber)
+
+alldat <- left_join(chambers,ThermTol_HeatPlate, by="file")
+which(alldat$chamber.x==25)
+all.equal(alldat$chamber.x, as.numeric(alldat$chamber.y))
+
+filt <- alldat[which(is.na(alldat$incapacitation)),]
+
+write_csv(filt, file="../ProcessedData/Val_nomatch.csv")
+
+
+
+#check for problem tracking
+
+plot(RNAi1$Rvar, RNAi1$incapacitation)
+
+plot(RNAi2$Rvar, RNAi2$incapacitation)
+
+
+R1_c <- subset(RNAi1, Rvar >75)
+R2_c <- subset(RNAi2, Rvar >75)
+
+proj <- "RNAi_1"
+TTdat <- readRDS(file=paste0("../ProcessedData/Combined_tracks_",proj,".Rds"))
+TTdat <- as.data.table(TTdat)
+ThermTol_HeatPlate <- readRDS(file=paste0("../ProcessedData/Incap_processed_",proj,".Rds"))
+
+pdf(file="../Plots/RNA1_score.pdf",width=8, height=12)
+par(mfrow=c(3,2))
+
+for(ff in R1_c$file)
+{
+  tt1 <- TTdat[TTdat$id==ff,]
+  tt1 <- tt1[tt1$likelihood >= 0.6,]
+  plot(tt1$Second, tt1$x, main=ff)
+  abline(v=ThermTol_HeatPlate[ThermTol_HeatPlate$file==ff,'incapacitation'])
+}
+
+dev.off()
+
+proj <- "RNAi_2"
+TTdat <- readRDS(file=paste0("../ProcessedData/Combined_tracks_",proj,".Rds"))
+TTdat <- as.data.table(TTdat)
+
+pdf(file="../Plots/RNA2_score.pdf",width=8, height=12)
+par(mfrow=c(3,2))
+
+for(ff in R2_c$file)
+{
+tt1 <- TTdat[TTdat$id==ff,]
+tt1 <- tt1[tt1$likelihood >= 0.6,]
+plot(tt1$Second, tt1$x, main=ff)
+abline(v=ThermTol_HeatPlate[ThermTol_HeatPlate$file==ff,'incapacitation'])
+}
+
+dev.off()
+
+
+#check random set
+pdf(file="../Plots/rand_score.pdf",width=8, height=12)
+par(mfrow=c(3,2))
+
+for(ff in sample(c(RNAi1$file,RNAi2$file),6*4))
+{
+  tt1 <- TTdat[TTdat$id==ff,]
+  tt1 <- tt1[tt1$likelihood >= 0.6,]
+  plot(tt1$Second, tt1$x, main=ff)
+  abline(v=ThermTol_HeatPlate[ThermTol_HeatPlate$file==ff,'incapacitation'])
+}
+
+dev.off()
+
+
