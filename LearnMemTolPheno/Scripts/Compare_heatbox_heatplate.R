2647c8e0d2a77a992c01ce8e619ec6fd9c553859
egking <kingeg@missouri.edu>
2022-07-01 17:30:43 -0500

    update script comparing heatbox and heatplate

---
+++ LearnMemTolPheno/Scripts/Compare_heatbox_heatplate.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+tt_plate <- readRDS("../ProcessedData/Incap_processed_VAL.Rds")
+
+tt_box <- read.table("../ProcessedData/ThermalTol_processed.txt",sep="\t", header=TRUE, stringsAsFactors = FALSE)
+
+
+plM <- tt_plate %>% group_by(genotype) %>%
+  summarise("Mtol" = mean(incapacitation))
+
+blM <- tt_box %>% group_by(patRIL) %>%
+  summarise("Mtol" = mean(incapacitation))
+blM$genotype <- as.character(blM$patRIL)
+
+allM <- inner_join(plM, blM, by="genotype")
+
+cor(allM$Mtol.x, allM$Mtol.y)
+plot(allM$Mtol.x[-9], allM$Mtol.y[-9])
